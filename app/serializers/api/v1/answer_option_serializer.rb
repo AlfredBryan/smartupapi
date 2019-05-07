ef16dc2950825cfc9068c92a162c4ac8b213a97c
MonsieurSlim <luccy559slim@gmail.com>
2019-05-07 10:33:49 +0100

    still breaking

--- app/serializers/api/v1/answer_option_serializer.rb
+++ app/serializers/api/v1/answer_option_serializer.rb
@@ -1,4 +1,3 @@
 class Api::V1::AnswerOptionSerializer < Api::V1::ApplicationSerializer
-  attribute :rank, :content
-  attribute :question_id, :correct
-end
+  attribute :rank, :content, :question_id
+end 
