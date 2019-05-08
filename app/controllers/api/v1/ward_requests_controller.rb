abe47df28a76aa822b9c3fea41a6f4f3af33d650
MonsieurSlim <luccy559slim@gmail.com>
2019-05-08 15:27:17 +0100

    ward request changes

--- app/controllers/api/v1/ward_requests_controller.rb
+++ app/controllers/api/v1/ward_requests_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::WardRequestsController < Api::V1::Resources::BaseController
   before_action :find_ward_request, except: [:index, :create]
-  skip_before_action :load_resource, only: :create
 
   def index
     @ward_requests = policy_scope(WardRequest)
@@ -8,17 +7,11 @@ class Api::V1::WardRequestsController < Api::V1::Resources::BaseController
   end
 
   def create
-    @ward_request = WardRequest.new()
     @ward_request.guardian = current_user
-    if @user = User.with_email(params[:email]).first
+    if @user = User.with_email(ward_request_params[:email]).first
       @ward_request.user = @user
     end
-    authorize(instance)
-    if instance.save && instance.valid?
-      render json: instance, serializer: serializer
-    else
-      render json: instance, serializer: serializer, status: :unprocessable_entity
-    end
+    super
   end
 
   def approve
@@ -30,7 +23,7 @@ class Api::V1::WardRequestsController < Api::V1::Resources::BaseController
   private
 
   def ward_request_params
-    params.require(:ward_request).permit(:id, :user_id, :guardian_id, :approved, :_destroy)
+    params.require(:ward_request).permit(:id, :user_id, :guardian_id, :approved, :_destroy, :email)
   end
 
   def find_ward_request
--- app/models/ward_request.rb
+++ app/models/ward_request.rb
@@ -1,4 +1,8 @@
 class WardRequest < ApplicationRecord
   belongs_to :user
   belongs_to :guardian, class_name: 'User', foreign_key: :guardian_id
+
+  validates :guardian, uniqueness: { scope: [:user_id, :approved], message: "Already has a pending ward request!" }
+
+  attr_accessor :email
 end
