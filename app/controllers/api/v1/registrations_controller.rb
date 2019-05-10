38afc095f002a9957ff5e5d1735db12a0bf07346
MonsieurSlim <luccy559slim@gmail.com>
2019-05-10 13:25:56 +0100

    changes registration

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -7,9 +7,9 @@ class Api::V1::RegistrationsController < Api::V1::BaseController
     @user = User.new(sign_up_params)
 
     if @user.save && @user.valid?
-      render(json: Api::V1::UserSerializer.new(@user).to_json.merge(status: 200))
+      render(json: Api::V1::UserSerializer.new(@user).to_h.merge(status: 200))
     else
-      render(json: Api::V1::UserSerializer.new(@user).to_json.merge(status: 400))
+      render(json: Api::V1::UserSerializer.new(@user).to_h.merge(status: 400))
     end
   end
 
@@ -20,9 +20,9 @@ class Api::V1::RegistrationsController < Api::V1::BaseController
   def update
     @user = current_user
     if @user.update_attributes(profile_params) && @user.valid?
-      render(json: Api::V1::UserSerializer.new(@user).to_json.merge(status: 200))
+      render(json: Api::V1::UserSerializer.new(@user).to_h.merge(status: 200))
     else
-      render(json: Api::V1::UserSerializer.new(@user).to_json.merge(status: 400))
+      render(json: Api::V1::UserSerializer.new(@user).to_h.merge(status: 400))
     end
   end
 
