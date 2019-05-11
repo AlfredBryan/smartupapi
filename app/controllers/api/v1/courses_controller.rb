d884dc164ffa7f80a39185fc4782bd13174641ed
MonsieurSlim <luccy559slim@gmail.com>
2019-05-11 18:37:35 +0100

    changes

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -4,8 +4,7 @@ class Api::V1::CoursesController < Api::V1::Resources::BaseController
   def index
     @institution = Institution.friendly.find(params[:institution_id]) if params[:institution_id]
     @resource_scope = @institution.courses if @institution
-    @resource_scope ||= Course.all
-    @courses = policy_scope(@resource_scope)
+    @courses = policy_scope(@resource_scope || Course)
     render json: @courses.map {|course| Api::V1::CourseSerializer.new(course).as_json}
   end
 
