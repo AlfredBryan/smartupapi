785381f0bd7672b97b9b0cf04c3fe14dbfebbbeb
MonsieurSlim <luccy559slim@gmail.com>
2019-05-11 18:27:19 +0100

    changes

--- app/serializers/api/v1/course_serializer.rb
+++ app/serializers/api/v1/course_serializer.rb
@@ -1,3 +1,3 @@
 class Api::V1::CourseSerializer < Api::V1::ApplicationSerializer
-  attributes :slug, :name, :description, :logo_url, :institution_id, :active, :creator_id
+  attributes :slug, :name, :description, :institution_id, :active, :creator_id
 end
