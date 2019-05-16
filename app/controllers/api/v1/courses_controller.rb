6ba83977dcfe251ed6b3635ad563109b3e981346
MonsieurSlim <luccy559slim@gmail.com>
2019-05-16 11:06:08 +0100

    changes for courses controller

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,14 +1,15 @@
 class Api::V1::CoursesController < Api::V1::Resources::BaseController
   before_action :find_course, except: [:index, :create]
+  before_action :find_institution, only: [:index, :create]
   skip_before_action :load_resource, except: :create
 
   def index
-    @institution = Institution.friendly.find(params[:institution_id]) if params[:institution_id]
     @courses = policy_scope((@institution.courses rescue nil) || Course.all)
     render json: @courses.map { |course|  Api::V1::CourseSerializer.new(course).as_json }
   end
 
   def create
+    @course.institution = @institution if @institution
     @course.creator = current_user
     super
   end
@@ -17,6 +18,10 @@ class Api::V1::CoursesController < Api::V1::Resources::BaseController
 
   def course_params
     params.require(:course).permit(:id, :name, :description, :institution_id, :active, :creator_id, :_destroy)
+  end
+
+  def find_institution
+    @institution = Institution.friendly.find(params[:institution_id]) if params[:institution_id]
   end
 
   def find_course
