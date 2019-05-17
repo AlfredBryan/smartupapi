94a07493762374ccad2dda0a2920718b499e06b2
MonsieurSlim <luccy559slim@gmail.com>
2019-05-17 12:23:34 +0100

    topics changes

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -1,10 +1,16 @@
 class Api::V1::TopicsController < Api::V1::Resources::BaseController
+  before_action :find_course
   before_action :find_topic, except: [:index, :create]
+  skip_before_action :load_resource, only: :create
 
   def index
-    @course = Course.friendly.find(params[:course_slug])
     @topics = policy_scope((@course.topics rescue Topic.none))
     render json: @topics.map {|topic| Api::V1::TopicSerializer.new(topic).as_json}
+  end
+
+  def create
+    @topic = @course.topics.new(topic_params)
+    super
   end
 
   private
@@ -13,8 +19,11 @@ class Api::V1::TopicsController < Api::V1::Resources::BaseController
     params.require(:topic).permit(:id, :name, :description, :rank, :course_id, :active, :_destroy)
   end
 
-  def find_topic
+  def find_course
     @course = Course.friendly.find(params[:course_slug])
+  end
+
+  def find_topic
     @topic = @course.topics.find(params[:id])
   end
 
