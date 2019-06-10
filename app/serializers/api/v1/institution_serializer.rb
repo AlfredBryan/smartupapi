1c53cc9173f3715484604264575088e9d58d3b2b
MonsieurSlim <luccy559slim@gmail.com>
2019-06-10 08:04:29 +0100

    image urls

--- app/serializers/api/v1/institution_serializer.rb
+++ app/serializers/api/v1/institution_serializer.rb
@@ -1,3 +1,7 @@
 class Api::V1::InstitutionSerializer < Api::V1::ApplicationSerializer
-  attributes :slug, :name, :motto, :logo, :email, :phone, :owner_id
+  attributes :slug, :name, :motto, :logo_url, :email, :phone, :owner_id
+
+  def logo_url
+    logo.url
+  end
 end
--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,6 +1,10 @@
 class Api::V1::UserSerializer < Api::V1::ApplicationSerializer
   attributes  :email, :status, :authentication_token,
               :first_name, :surname, :address, :phone,
-              :image, :state, :full_name, :admin,
+              :image_url, :state, :full_name, :admin,
               :sex, :level, :age, :date_of_birth, :completed_at
+
+  def image_url
+    image.url
+  end
 end
