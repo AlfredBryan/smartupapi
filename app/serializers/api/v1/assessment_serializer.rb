1c4c3273122ad872a2b4e315d9d33bec87d3a28c
MonsieurSlim <luccy559slim@gmail.com>
2019-06-16 15:16:25 +0100

    changes

--- app/serializers/api/v1/assessment_serializer.rb
+++ app/serializers/api/v1/assessment_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::AssessmentSerializer < Api::V1::ApplicationSerializer
-  attributes :name, :description, :course, :questions
+  attributes :name, :description, :course, :assessment_questions, :questions
 
   def questions
     ActiveModel::SerializableResource.new(object.questions,  each_serializer: Api::V1::QuestionSerializer)
