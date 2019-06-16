2738aff9a50d7d1ba3166fe7d9d0b8998d1833b4
MonsieurSlim <luccy559slim@gmail.com>
2019-06-16 14:04:33 +0100

    fix

--- app/serializers/api/v1/assessment_serializer.rb
+++ app/serializers/api/v1/assessment_serializer.rb
@@ -2,6 +2,6 @@ class Api::V1::AssessmentSerializer < Api::V1::ApplicationSerializer
   attributes :name, :description, :course, :questions
 
   def questions
-    ActiveModel::SerializableResource.new(object.questions,  each_serializer: QuestionSerializer)
+    ActiveModel::SerializableResource.new(object.questions,  each_serializer: Api::V1::QuestionSerializer)
   end
 end
--- app/serializers/api/v1/question_serializer.rb
+++ app/serializers/api/v1/question_serializer.rb
@@ -2,6 +2,6 @@ class Api::V1::QuestionSerializer < Api::V1::ApplicationSerializer
   attributes :name, :description, :topic_id, :answer_options
 
   def answer_options
-    ActiveModel::SerializableResource.new(object.answer_options,  each_serializer: AnswerOptionSerializer)
+    ActiveModel::SerializableResource.new(object.answer_options,  each_serializer: Api::V1::AnswerOptionSerializer)
   end
 end
