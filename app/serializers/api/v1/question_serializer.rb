6bf4eba0cb5af1398e6bc62812a84cdd26792fa7
MonsieurSlim <luccy559slim@gmail.com>
2019-06-26 18:07:14 +0100

    change

--- app/serializers/api/v1/question_serializer.rb
+++ app/serializers/api/v1/question_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::QuestionSerializer < Api::V1::ApplicationSerializer
-  attributes :name, :description, :topic_id, :answer_options
+  attributes :name, :description, :topic_id, :answer_options, :question_type
 
   def answer_options
     ActiveModel::SerializableResource.new(object.answer_options,  each_serializer: Api::V1::AnswerOptionSerializer)
