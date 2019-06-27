9317e9345e261c090c373e009bdd70bf75946aaf
MonsieurSlim <luccy559slim@gmail.com>
2019-06-27 17:11:29 +0100

    changes

--- app/serializers/api/v1/answer_serializer.rb
+++ app/serializers/api/v1/answer_serializer.rb
@@ -1,3 +1,3 @@
 class Api::V1::AnswerSerializer < Api::V1::ApplicationSerializer
-  attributes :state, :content, :content_url, :question_id, :assessment_id, :answer_option_id, :user_id
+  attributes :state, :content, :content_url, :question, :assessment_id, :answer_option_id, :user_id
 end
