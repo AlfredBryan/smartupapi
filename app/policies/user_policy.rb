b03001c066c1a2313bd6f9f0c73617c19dba1055
MonsieurSlim <luccy559slim@gmail.com>
2019-07-01 08:28:52 +0100

    fixes

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,27 @@
+class QuestionPolicy < ApplicationPolicy
+  class Scope < Struct.new(:user, :scope)
+    def resolve
+      if user.admin?
+        scope
+      elsif user.educator?
+        scope.where(id: user.group_memberships.pluck(:user_id).flatten)
+      elsif user.guardian?
+        scope.where(id: user.ward_ids)
+      else
+        scope.where(id: user.guardian_ids)
+      end
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    admin? || (user == record)
+  end
+
+  def destroy?
+    update?
+  end
+end
--- app/serializers/api/v1/study_group_serializer.rb
+++ app/serializers/api/v1/study_group_serializer.rb
@@ -1,5 +1,9 @@
 class Api::V1::StudyGroupSerializer < Api::V1::ApplicationSerializer
-  attributes :name, :level, :members, :memberships
+  attributes :name, :level, :members, :memberships, :owner_id
+
+  def owner_id
+    object.institution.owner_id
+  end
 
   def memberships
     ActiveModel::SerializableResource.new(object.group_memberships,  each_serializer: Api::V1::GroupMembershipSerializer)
