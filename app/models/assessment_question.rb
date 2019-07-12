5f9e1c090e743f1b697e17feb4bad90538fdbcb7
MonsieurSlim <luccy559slim@gmail.com>
2019-07-12 18:42:17 +0100

    changes

--- app/models/assessment_question.rb
+++ app/models/assessment_question.rb
@@ -3,4 +3,10 @@ class AssessmentQuestion < ApplicationRecord
   belongs_to :question
 
   validates :question_id, uniqueness: { scope: :assessment_id, message: "This question has already been added!" }
+
+  def max_score
+    question.max_score(assessment_id)
+  rescue
+    0
+  end
 end
--- app/serializers/api/v1/assessment_question_serializer.rb
+++ app/serializers/api/v1/assessment_question_serializer.rb
@@ -1,3 +1,7 @@
 class Api::V1::AssessmentQuestionSerializer < Api::V1::ApplicationSerializer
-  attributes :assessment_id, :question
+  attributes :assessment_id, :question, :max_score
+
+  def max_score
+    object.max_score
+  end
 end
