abf7e1fcd2982b78722454e9d964a52d0eae8277
MonsieurSlim <luccy559slim@gmail.com>
2019-07-16 11:25:38 +0100

    changes

--- app/serializers/api/v1/assessment_result_serializer.rb
+++ app/serializers/api/v1/assessment_result_serializer.rb
@@ -1,6 +1,10 @@
 class Api::V1::AssessmentResultSerializer < Api::V1::ApplicationSerializer
   attributes :user, :assessment, :score, :status, :answers, :grade
 
+  def answers
+    ActiveModel::SerializableResource.new(object.answers,  each_serializer: Api::V1::AnswerSerializer)
+  end
+
   def grade
     object.grade
   end
