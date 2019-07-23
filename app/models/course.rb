d9a1929644111018c059ce20e273388aabfef245
MonsieurSlim <luccy559slim@gmail.com>
2019-07-23 14:21:37 +0100

    changes

--- app/models/course.rb
+++ app/models/course.rb
@@ -13,6 +13,11 @@ class Course < ApplicationRecord
 
 
   def self.import(file, creator_id, institution=nil)
+    puts "#########################"
+    puts "#########################"
+    puts file
+    puts "#########################"
+    puts "#########################"
     CSV.foreach(file.path, headers: true) do |row|
       course_hash = {}
       course_hash[:creator_id] = creator_id
